2e6dac1306f56f6cc34a7d3c21e2c8e4ee034da2
Amanuel Abera <amanuelabera46@gmail.com>
2024-08-06 17:48:59 +0300

    roll back the credentials

--- server/middleware/credentials.js
+++ server/middleware/credentials.js
@@ -4,12 +4,6 @@ const credentials = (req, res, next) => {
   const origin = req.headers.origin;
 
   if (allowedOrigins.includes(origin)) {
-    // res.setHeader('Access-Control-Allow-Credentials', 'true');
-    // res.setHeader(
-    //   'Access-Control-Allow-Headers',
-    //   'Content-Type, Authorization'
-    // );
-    res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader(
       'Access-Control-Allow-Headers',
