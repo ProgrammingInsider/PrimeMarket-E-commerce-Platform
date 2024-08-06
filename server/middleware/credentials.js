5833112524f24d0cb3423af9c61ecd856098b39f
Amanuel Abera <amanuelabera46@gmail.com>
2024-08-06 17:26:39 +0300

    update allowed origion

--- server/middleware/credentials.js
+++ server/middleware/credentials.js
@@ -4,6 +4,12 @@ const credentials = (req, res, next) => {
   const origin = req.headers.origin;
 
   if (allowedOrigins.includes(origin)) {
+    // res.setHeader('Access-Control-Allow-Credentials', 'true');
+    // res.setHeader(
+    //   'Access-Control-Allow-Headers',
+    //   'Content-Type, Authorization'
+    // );
+    res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader(
       'Access-Control-Allow-Headers',
