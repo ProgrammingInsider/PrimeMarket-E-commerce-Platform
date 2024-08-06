e6b5e5d038d591e32854145c11f2a29901bdf67a
Amanuel Abera <amanuelabera46@gmail.com>
2024-08-06 15:28:55 +0300

    update allowed origins

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://primemarket.onrender.com'
+];
 
 export default allowedOrigins;
