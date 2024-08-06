cad65531c009f28eec7a58387a754920539878aa
Amanuel Abera <amanuelabera46@gmail.com>
2024-08-06 15:12:21 +0300

    ready for deploy 1

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./assets/styles/global.css";
 import ContextAPI from "./context/ContextAPI.jsx";
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (import.meta.env.PROD) {
+    disableReactDevTools();
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
