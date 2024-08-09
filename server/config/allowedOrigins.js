d4f9d110194042b54f5758b413cbc2fe5e5bff70
Amanuel Abera <amanuelabera46@gmail.com>
2024-08-09 21:09:55 +0300

    adding allowed origin for vercel

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
-    'https://primemarket.onrender.com'
+    'https://primemarket.onrender.com',
+    'primemarket.vercel.app'
 ];
 
 export default allowedOrigins;
