86a2ef9ddffcfc3b4e54d09a188e8bc5a968342f
Amanuel Abera <amanuelabera46@gmail.com>
2024-08-09 21:25:54 +0300

    update allowed origin

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'https://primemarket.onrender.com',
-    'https://primemarket.vercel.app/'
+    'https://primemarket.vercel.app/',
+    'https://prime-market-e-commerce-platform-5mkl8dt2l.vercel.app/'
 ];
 
 export default allowedOrigins;
