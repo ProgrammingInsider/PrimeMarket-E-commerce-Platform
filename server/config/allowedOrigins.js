3aab0548b4d43883ea8739835a4caaba6628ff7a
Amanuel Abera <amanuelabera46@gmail.com>
2024-08-20 18:40:51 +0300

    Adding CI github action file

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'http://localhost:5173',
     'https://primemarket.onrender.com',
     'https://primemarket.vercel.app/',
-    'https://prime-market-e-commerce-platform-5mkl8dt2l.vercel.app/'
+    'https://prime-market-e-commerce-platform-5mkl8dt2l.vercel.app/',
+    'https://primemarket-e-commerce-platform-1.onrender.com'
 ];
 
 export default allowedOrigins;
