1980c07ee74ebdf2a34c2f2ee933e47b7350e1e1
Amanuel Abera <amanuelabera46@gmail.com>
2024-08-22 16:30:21 +0300

    updated PORT options

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import app from './app.js';
 
 // Start the server
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`the app listening on ${PORT} port`);
