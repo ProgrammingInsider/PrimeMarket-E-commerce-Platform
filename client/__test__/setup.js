f55f0819f0b57a6ea2292c0d2cee28b774d362be
Amanuel Abera <amanuelabera46@gmail.com>
2024-08-23 12:47:21 +0300

    updated setup.js

--- client/__test__/setup.js
+++ client/__test__/setup.js
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom/vitest";
-import { afterEach, beforeEach, vi } from "vitest";
+import { vi } from "vitest";
 
 // Clear all mocks after each test
 beforeEach(() => {
